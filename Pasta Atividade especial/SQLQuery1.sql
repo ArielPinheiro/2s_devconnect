c6fb47857dc4a29d4694657ba4881262acdd1f12
Ariel <arielolivapinheirodosantos@gmail.com>
2025-10-08 09:01:11 -0300

    pasta nova

---
+++ Pasta Atividade especial/SQLQuery1.sql
@@ -0,0 +1,25 @@
+--DML
+USE db_devconnect;
+INSERT INTO tb_Usuario(NomeCompleto, NomeUsuario, Email, Senha, FotoPerfilUrl)
+VALUES('Ronaldo', 'Ronaldinho', 'Ronaldinho@gmail.com', 'Ronaldo123', 'www.img.url');
+
+
+INSERT INTO tb_Usuario(NomeCompleto, NomeUsuario, Email, Senha, FotoPerfilUrl)
+VALUES('Ariel S', 'Ariel', 'Ariel@gmail.com', 'Ariel2008', 'www.img.url');
+
+
+SELECT * FROM tb_Usuario;
+
+
+INSERT INTO tb_Publicacao(Descricao, ImagemUrl, DataPublicacao, id_usuario)
+VALUES('Jogador De Fut', 'Imagem.Img', '2020/07/22', 4);
+
+INSERT INTO tb_Curtida(id_Usuario, id_Publicacao)
+VALUES(4, 3);
+
+INSERT INTO tb_Comentario(Texto, DataComentario, id_Usuario, id_Publicacao)
+VALUES('Adoro Fut', '2021/09/23', 4, 3);
+
+
+INSERT INTO tb_Seguidor(id_Usuario_Seguido, id_Usuario_Seguidor)
+VALUES(2,5);
